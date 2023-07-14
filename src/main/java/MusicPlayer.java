7f8f4c2921ad65c5e9e03d0285666c0256dfd9ac
Daniel Commins <daniel.commins@techelevator.com>
2023-07-13 23:07:15 -0700

    add new song :)

--- src/main/java/MusicPlayer.java
+++ src/main/java/MusicPlayer.java
@@ -29,17 +29,65 @@ public class MusicPlayer {
          */
         try {
 
-            playNote("4C", 1000);
-            rest(500);
-            playNote("4D", MusicApp.MAX_VOLUME, 1000);
-            playNote("4D#", 500);
-            rest(600);
+            twinkle();
+
+// Other examples:
+//            playNote("C4", 1000);
+//            rest(500);
+//            playNote("D4", MusicApp.MAX_VOLUME, 1000);
+//            playNote("D#4", 500);
+//            rest(600);
 
         } catch(InterruptedException e) {
             e.printStackTrace();
         }
 
         closeSynth();
+    }
+
+    public void twinkle() throws InterruptedException {
+        rest(100);
+        twinkle1();
+        rest(500);
+        twinkle2();
+        rest(500);
+        for(int i = 1; i < 2; i++){
+            twinkle3();
+            rest(500);
+        }
+        twinkle1();
+        rest(500);
+        twinkle2();
+    }
+    public void twinkle1() throws InterruptedException {
+        playNote("G4");
+        playNote("G4");
+        playNote("D4");
+        playNote("D4");
+        playNote("E4");
+        playNote("E4");
+        playNote("D4");
+        System.out.println();
+    }
+    public void twinkle2() throws InterruptedException {
+        playNote("C4");
+        playNote("C4");
+        playNote("B4");
+        playNote("B4");
+        playNote("A4");
+        playNote("A4");
+        playNote("G4");
+        System.out.println();
+    }
+    public void twinkle3() throws InterruptedException {
+        playNote("D4");
+        playNote("D4");
+        playNote("C4");
+        playNote("C4");
+        playNote("B4");
+        playNote("B4");
+        playNote("A4");
+        System.out.println();
     }
 
     /*
@@ -50,16 +98,21 @@ public class MusicPlayer {
     }
 
     /*
-     * Play a note in the form <octave><note>, e.g. "4C" for middle C
+     * Play a note in the form <octave><note>, e.g. "C4" for middle C
      * https://docs.oracle.com/en/java/javase/11/docs/api/java.desktop/javax/sound/midi/MidiChannel.html#noteOn(int,int)
      */
+    private void playNote(String note) throws InterruptedException {
+        System.out.print(note + " " );
+        playNote(note, 500);
+    }
+
     private void playNote(String note, int durationMs) throws InterruptedException {
         playNote(note, this.volume, durationMs);
     }
 
     private void playNote(String note, int volume, int durationMs) throws InterruptedException {
-        int octave = Integer.parseInt(note.substring(0, 1));
-        int tone = getToneIndex(note.substring(1));
+        int octave = Integer.parseInt(note.substring(note.length() - 1));
+        int tone = note.length() > 2 ? getToneIndex(note.substring(0, 2)) : getToneIndex(note.substring(0, 1));
         int midi_note = 12 + (12 * octave) + tone;
 
         /*
